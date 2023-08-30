d41bc681fb9ab5e958ce659957ae5f88bd8d61b6
Onosgb <onosgb@gmail.com>
2023-08-30 16:49:47 +0100

    added another method to the prime algorithms

--- 33-find-prime-number.js
+++ 33-find-prime-number.js
@@ -15,11 +15,36 @@ is_prime(2)
 is_prime(-1)
  */
 
+// method 1;
 function isPrime(num) {
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  //TODO
+
+  // check if num less or equl 1
+  if (num <= 1) return false;
+
+  // check if 3 less than or equal 3
+  if (num <= 3) return true;
+
+  // check num modules equl 0 return false
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+
+  return true;
+}
+
+// method 2;
+function isPrime2(num) {
+  if (num < 2) return false;
+
+  const limit = Math.sqrt(num);
+
+  for (let i = 2; i <= limit; ++i) {
     if (num % i === 0) {
       return false;
     }
   }
-  return num > 1;
+  return true;
 }
