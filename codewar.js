5f12492d9e9c7b92496ca936929adc4312502c6f
Onosgb <onosgb@gmail.com>
2023-09-04 11:09:13 +0100

    RecoverSecret: Algorithms to recover secrete

--- codewar.js
+++ codewar.js
@@ -1,35 +1,26 @@
-/*
-Take the following IPv4 address: 128.32.10.1
-
-This address has 4 octets where each octet is a single byte (or 8 bits).
-
-1st octet 128 has the binary representation: 10000000
-2nd octet 32 has the binary representation: 00100000
-3rd octet 10 has the binary representation: 00001010
-4th octet 1 has the binary representation: 00000001
-So 128.32.10.1 == 10000000.00100000.00001010.00000001
-
-Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
-
-Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
-
-Examples
-2149583361 ==> "128.32.10.1"
-32         ==> "0.0.0.32"
-0          ==> "0.0.0.0"
-*/
-
-function int32ToIp(int32) {
-  let binaryStr = int32.toString(2).padStart(32, "0");
-
-  let octets = [];
-  for (let i = 0; i < 32; i += 8) {
-    octets.push(parseInt(binaryStr.slice(i, i + 8), 2));
+var recoverSecret = function (triplets) {
+  for (var [first] of triplets) {
+    if (triplets.every((tuple) => tuple.indexOf(first) <= 0)) {
+      triplets
+        .filter(([item]) => item == first)
+        .forEach((tuple) => tuple.shift());
+      return (
+        first + recoverSecret(triplets.filter((tuple) => tuple.length > 0))
+      );
+    }
   }
+  return "";
+};
 
-  return octets.join(".");
-}
-
-let result = int32ToIp(2149583361);
-
-console.log("results: ", result);
+// Example usage:
+const triplets = [
+  ["t", "u", "p"],
+  ["w", "h", "i"],
+  ["t", "s", "u"],
+  ["a", "t", "s"],
+  ["h", "a", "p"],
+  ["t", "i", "s"],
+  ["w", "o", "r"],
+  ["t", "s", "r"],
+];
+console.log(recoverSecret(triplets)); // Output: "whatisup"
